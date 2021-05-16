992c339b316ed27b15b2641a322cd8a21e1c3c90
fireztron <84250829+fireztron@users.noreply.github.com>
2021-05-16 18:13:25 -0400

    Update autofarm.lua

--- autofarm.lua
+++ autofarm.lua
@@ -32,7 +32,7 @@ function TPReturner()
         foundAnything = Site.nextPageCursor
     end
     local num = 0;
-    print"getting tp id"
+    print("getting tp id" .. iterations)
     if iterations > 500 then
         local delFile = pcall(function()
 	    delfile("NotSameServers.json")
