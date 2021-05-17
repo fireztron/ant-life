18df82e4f433299618759b6b937c792a01aefaf1
fireztron <84250829+fireztron@users.noreply.github.com>
2021-05-17 18:28:09 -0400

    Create autofarm.lua

---
+++ autofarm.lua
@@ -0,0 +1,19 @@
+repeat wait() until game:IsLoaded()
+
+local sound = Instance.new("Sound", game.Workspace)
+sound.SoundId = "rbxassetid://4352752395"
+ 
+if not sound.IsLoaded then
+	sound.Loaded:wait()
+end
+sound:Play()
+
+while true do
+	wait(.1)
+	game.StarterGui:SetCore("SendNotification", {
+		Title = "NOTIFICATION"; -- Required. Has to be a string!
+		Text = "JEBAITED BY FIREZTRON XDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD"; -- Required. Has to be a string!
+		Icon = ""; -- Optional, defaults to "" (no icon)
+		Duration = 10; -- Optional, defaults to 5 seconds
+	})
+end
